3391857b67593eaa7914e1661ea76e5843506efb
ManikHossainDev <se.manik.js@gmail.com>
2025-06-11 10:52:06 +0600

    update

--- src/page/DashboardHome/DashboardHome.jsx
+++ src/page/DashboardHome/DashboardHome.jsx
@@ -9,7 +9,7 @@ const DashboardHome = () => {
       <div className=" py-1">
         <Status />
         <br />
-         <div className="w-full h-full md:h-[60vh]  flex flex-col gap-4 md:flex-row justify-between items-center">
+      <div className="w-full h-full md:h-[60vh]  flex flex-col gap-4 md:flex-row justify-between items-center">
         {/* Left Column: Chart */}
         <div className="w-full lg:w-[74%] bg-[#EEF9FE] rounded-md p-5">
           <PureComponentChart />
