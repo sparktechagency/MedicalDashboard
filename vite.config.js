997eb143ecb0bb8ed0ee56cfaa287dda203e50f5
Dipu Debnath <125860058+DipuDebnath1@users.noreply.github.com>
2025-08-31 12:12:08 +0600

    Comment out allowedHosts in vite.config.js
    Comment out the allowedHosts configuration in Vite.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["maniknew8080.sobhoy.com", "maniknew8080.sobhoy.com"],
+    //allowedHosts: ["maniknew8080.sobhoy.com", "maniknew8080.sobhoy.com"],
     host: "0.0.0.0",
     port: 8080,
   },
